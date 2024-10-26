4722a8c91074a4d291356ce6b1ea351cf74794fe
myChinesePYL <3223285598@qq.com>
2024-10-26 15:05:17 +0800

    保存一下

--- src/object.rs
+++ src/object.rs
@@ -1,17 +1,40 @@
 pub struct VData {
-    position: glm::Vector3<f64>,
-    viewpoint: glm::Vector3<f64>,
+    pub position: glm::Vector3<f64>,
+    pub viewpoint: glm::Vector3<f64>,
     surface: [glm::Vector3<f64>; 3],
 }
 
+impl VData {
+    pub fn new(position: glm::Vector3<f64>, viewpoint: glm::Vector3<f64>, surface: [glm::Vector3<f64>; 3]) -> Self {
+        Self {
+            position,
+            viewpoint,
+            surface,
+        }
+    }
+    pub fn set_surface(&mut self, index: usize, position: glm::Vector3<f64>) {
+        *self.surface.get_mut(index).unwrap() = position;
+    }
+    pub fn get_surface(&self, index: usize) -> &glm::Vector3<f64> {
+        self.surface.get(index).unwrap()
+    }
+}
+
 pub struct Object {
-    position: glm::Vector3<f64>,
-    rotation: glm::Vector3<f64>,
+    pub position: glm::Vector3<f64>,
+    pub rotation: glm::Vector3<f64>,
     vdata: Vec<VData>,
 }
 
 impl Object {
-    pub fn new(pos: glm::Vector3<f64>, rot: glm::Vector3<f64>) -> Self {
-        Self { position: pos, rotation: rot, vdata: Vec::new() }
+    pub fn new(position: glm::Vector3<f64>, rotation: glm::Vector3<f64>) -> Self {
+        Self {
+            position,
+            rotation,
+            vdata: Vec::new(),
+        }
+    }
+    pub fn add_vertex(&mut self,v: VData) {
+        self.vdata.push(v);
     }
 }
